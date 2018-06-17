0ef8f73728cccf7c1eb738318c8a590284324c46
Emilio Ochoa <emilioaor@gmail.com>
2018-06-17 18:01:28 -0400

    Migracion para columna patient_reference_id

--- database/migrations/2018_06_17_175954_patient_reference_id_column.php
+++ database/migrations/2018_06_17_175954_patient_reference_id_column.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class PatientAddColumnPatientReferenceId extends Migration
+class PatientReferenceIdColumn extends Migration
 {
     /**
      * Run the migrations.

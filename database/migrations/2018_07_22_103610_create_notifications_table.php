adf59484af7d23afc11f983a531cc4d6c8bec65b
Emilio Ochoa <emilioaor@gmail.com>
2018-07-22 15:32:59 -0400

    Corrigiendo migracion

--- database/migrations/2018_07_22_103610_create_notifications_table.php
+++ database/migrations/2018_07_22_103610_create_notifications_table.php
@@ -13,6 +13,8 @@ class CreateNotificationsTable extends Migration
      */
     public function up()
     {
+        Schema::defaultStringLength(191);
+
         Schema::create('notifications', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('type');

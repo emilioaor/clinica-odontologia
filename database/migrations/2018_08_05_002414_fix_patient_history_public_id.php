0cafe8e20000baf7a3171d7c2a7b6b65349182b1
Emilio Ochoa <emilioaor@gmail.com>
2018-08-05 00:34:00 -0400

    Corrigiendo public_id null en historial de paciente

---
+++ database/migrations/2018_08_05_002414_fix_patient_history_public_id.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixPatientHistoryPublicId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $services = \App\PatientHistory::query()->whereNull('public_id')->get();
+
+        $c = 0;
+
+        foreach ($services as $service) {
+            $c++;
+
+            $service->public_id = 'SERF' . $c;
+            $service->save();
+        }
+
+        Schema::table('patient_history', function (Blueprint $table) {
+            $table->string('public_id', 30)->nullable(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

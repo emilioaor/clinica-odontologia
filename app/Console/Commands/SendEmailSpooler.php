51227c039fb73cc420f4e42ba31ef6083b3ca00d
Emilio Ochoa <emilioaor@gmail.com>
2018-11-01 22:44:36 -0400

    Cambio en storage para rayos x

--- app/Console/Commands/SendEmailSpooler.php
+++ app/Console/Commands/SendEmailSpooler.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\EmailSpooler;
+use App\RayX;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Mail;
 
@@ -54,6 +55,33 @@ class SendEmailSpooler extends Command
 
             $email->status = EmailSpooler::STATUS_COMPLETE;
             $email->save();
+        }
+
+        $this->imageStorage();
+    }
+
+    private function imageStorage()
+    {
+        $imageToStorage = RayX::query()->whereNotNull('base64')->get();
+
+        foreach ($imageToStorage as $image) {
+
+            $upload = base64_decode($image->base64);
+            $path = public_path($image->url);
+
+            $dir = explode('/', $image->url);
+            unset($dir[ count($dir) - 1 ]);
+            $dir = implode('/', $dir);
+            $dir = public_path($dir);
+
+            if (! is_dir($dir)) {
+                mkdir($dir);
+            }
+
+            file_put_contents($path, $upload);
+
+            $image->base64 = null;
+            $image->save();
         }
     }
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,8 +15,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        SendEmailSpooler::class,
-        ImageStorage::class
+        SendEmailSpooler::class
     ];
 
     /**
